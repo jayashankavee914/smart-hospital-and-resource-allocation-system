76070503283e1912758a47390213fd77df6210a2
jayashan.kavee914 <jayashan.kavee914@gmail.com>
2026-09-18 18:59:09 +0530

    Add Display all patients data function

---
+++ patientDisplay.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "hospitalData.h"
+#include "patientDisplay.h"
+
+void displayPatients(patientData patients[], int patientCount)
+{
+    if (patientCount == 0)
+    {
+        printf("\nNo patients registered.\n");
+        return;
+    }
+
+    printf("\n========== REGISTERED PATIENTS ==========\n");
+
+    for (int i = 0; i < patientCount; i++)
+    {
+        printf("\nPatient %d\n", i + 1);
+        printf("-----------------------------------------\n");
+
+        printf("Patient ID       : %s\n", patients[i].patientID);
+        printf("Patient Age      : %d\n", patients[i].patientAge);
+        printf("Triage Level     : %d\n", patients[i].triageLevel);
+        printf("Specialty ID     : %d\n", patients[i].specialtyID);
+        printf("Ward Status      : %d\n", patients[i].wardStatus);
+        printf("Ward ID          : %d\n", patients[i].wardID);
+        printf("Days Admitted    : %d\n", patients[i].daysAdmitted);
+    }
+
+    printf("\n=========================================\n");
+}
---
+++ patientDisplay.h
@@ -0,0 +1,8 @@
+#ifndef PATIENTDISPLAY_H
+#define PATIENTDISPLAY_H
+
+#include "hospitalData.h"
+
+void displayPatients(patientData patients[], int patientCount);
+
+#endif
